1b091bcd356126700b91b7432deb31dfe6fe74a5
Kiichi Tachibana <bodega1123@gmail.com>
2020-04-04 19:01:53 +0900

    Style markers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import Helmet from 'react-helmet';
-import L from 'leaflet';
+import L, { latLng } from 'leaflet';
 import axios from 'axios';
 
 import Layout from 'components/Layout';
@@ -8,8 +8,8 @@ import Container from 'components/Container';
 import Map from 'components/Map';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -33,7 +33,7 @@ const IndexPage = () => {
 
       const geoJson = {
         type: 'FeatureCollection',
-        feature: countries.map((country = {}) => {
+        features: countries.map((country = {}) => {
           const { countryInfo = {} } = country;
           const { lat, long: lng } = countryInfo;
           return {
@@ -49,6 +49,51 @@ const IndexPage = () => {
         }),
       };
       console.log(geoJson);
+
+      const geoJsonLayers = new L.GeoJSON(geoJson, {
+        pointToLayer: (feature = {}, latlng) => {
+          const { properties = {} } = feature;
+          let updatedFormatted;
+          let casesString;
+
+          const { country, updated, cases, deaths, recovered } = properties;
+
+          casesString = `${cases}`;
+
+          if (cases > 1000) {
+            casesString = `${casesString.slice(0, -3)}k+`;
+          }
+
+          if (updated) {
+            updatedFormatted = new Date(updated).toLocaleString();
+          }
+
+          const html = `
+          <span class=“icon-marker”>
+            <span class=“icon-marker-tooltip”>
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+          return L.marker(latlng, {
+            icon: L.divIcon({
+              className: 'icon',
+              html,
+            }),
+            riseOnHover: true,
+          });
+        },
+      });
+
+      geoJsonLayers.addTo(map);
     } catch (e) {
       console.error(`Failed to fetch countries: ${e.message}, e`);
     }
